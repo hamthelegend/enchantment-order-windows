5326693af164a30ed45fe77b68f1a38c16ee9c7a
F i s h <108134885+Er0sVal@users.noreply.github.com>
2022-06-25 14:05:21 +0800

    Update ItemType.cs

--- BusinessLogic/ItemType.cs
+++ BusinessLogic/ItemType.cs
@@ -10,10 +10,10 @@ public record ItemType(
         CompatibleEnchantmentTypes: EnchantmentType.All,
         DefaultEnchantmentTypes: new List<EnchantmentType>());
     
-    public ItemType static ItemType Helmet = new(
-        FriendlyName: "Helmet",
-        CompatibleEnchantmentTypes: new List<EnchantmentType>()
-        {
+    public static ItemType Helmet = new(
+       FriendlyName: "Helmet",
+       CompatibleEnchantmentTypes: new List<EnchantmentType>() 
+       {
             EnchantmentType.AquaAffinity,
             EnchantmentType.BlastProtection,
             EnchantmentType.CurseOfBinding,
@@ -25,14 +25,360 @@ public record ItemType(
             EnchantmentType.Respiration,
             EnchantmentType.Thorns,
             EnchantmentType.Unbreaking
-        },
-        DefaultEnchantmentTypes: new List<EnchantmentType>()
-        {
+       },
+       DefaultEnchantmentTypes: new List<EnchantmentType>()
+       {
             EnchantmentType.AquaAffinity,
             EnchantmentType.Mending,
             EnchantmentType.Protection,
             EnchantmentType.Respiration,
             EnchantmentType.Thorns,
+            EnchantmentType.Unbreaking
+       });
+
+    public static ItemType Chestplate = new(
+       FriendlyName: "Chestplate",
+       CompatibleEnchantmentTypes: new List<EnchantmentType>()
+       {
+            EnchantmentType.BlastProtection,
+            EnchantmentType.CurseOfBinding,
+            EnchantmentType.CurseOfVanishing,
+            EnchantmentType.FireProtection,
+            EnchantmentType.Mending,
+            EnchantmentType.ProjectileProtection,
+            EnchantmentType.Protection,
+            EnchantmentType.Thorns,
+            EnchantmentType.Unbreaking
+       },
+       DefaultEnchantmentTypes: new List<EnchantmentType>()
+       {
+            EnchantmentType.Mending,
+            EnchantmentType.Protection,
+            EnchantmentType.Thorns,
+            EnchantmentType.Unbreaking
+       });
+
+    public static ItemType Leggings = new(
+       FriendlyName: "Leggins",
+       CompatibleEnchantmentTypes: new List<EnchantmentType>()
+       {
+                EnchantmentType.BlastProtection,
+                EnchantmentType.CurseOfBinding,
+                EnchantmentType.CurseOfVanishing,
+                EnchantmentType.FireProtection,
+                EnchantmentType.Mending,
+                EnchantmentType.ProjectileProtection,
+                EnchantmentType.Protection,
+                EnchantmentType.Thorns,
+                EnchantmentType.Unbreaking,
+                EnchantmentType.SwiftSneak
+       },
+       DefaultEnchantmentTypes: new List<EnchantmentType>()
+       {
+                EnchantmentType.Mending,
+                EnchantmentType.Protection,
+                EnchantmentType.SwiftSneak,
+                EnchantmentType.Thorns,
+                EnchantmentType.Unbreaking
+       });
+
+    public static ItemType Boots = new(
+       FriendlyName: "Boots",
+       CompatibleEnchantmentTypes: new List<EnchantmentType>()
+       {
+                EnchantmentType.BlastProtection,
+                EnchantmentType.CurseOfBinding,
+                EnchantmentType.CurseOfVanishing,
+                EnchantmentType.DepthStrider,
+                EnchantmentType.FeatherFalling,
+                EnchantmentType.FireProtection,
+                EnchantmentType.FrostWalker,
+                EnchantmentType.Mending,
+                EnchantmentType.ProjectileProtection,
+                EnchantmentType.Protection,
+                EnchantmentType.SoulSpeed,
+                EnchantmentType.Thorns,
+                EnchantmentType.Unbreaking
+       },
+       DefaultEnchantmentTypes: new List<EnchantmentType>()
+       {
+                EnchantmentType.DepthStrider,
+                EnchantmentType.FeatherFalling,
+                EnchantmentType.Mending,
+                EnchantmentType.Protection,
+                EnchantmentType.SoulSpeed,
+                EnchantmentType.Thorns,
+                EnchantmentType.Unbreaking
+       });
+
+    public static ItemType Elytra = new(
+       FriendlyName: "Elytra",
+       CompatibleEnchantmentTypes: new List<EnchantmentType>()
+       {
+                EnchantmentType.CurseOfBinding,
+                EnchantmentType.CurseOfVanishing,
+                EnchantmentType.Mending,
+                EnchantmentType.Unbreaking
+       },
+       DefaultEnchantmentTypes: new List<EnchantmentType>()
+       {
+                EnchantmentType.Mending,
+                EnchantmentType.Unbreaking
+       });
+
+    public static ItemType Head = new(
+       FriendlyName: "Head",
+       CompatibleEnchantmentTypes: new List<EnchantmentType>()
+       {
+                EnchantmentType.CurseOfBinding,
+                EnchantmentType.CurseOfVanishing,
+
+       },
+   DefaultEnchantmentTypes: new List<EnchantmentType>()
+       {
+                EnchantmentType.CurseOfBinding,
+       });
+    
+    public static ItemType Sword = new(
+        FriendlyName: "Sword",
+        CompatibleEnchantmentTypes: new List<EnchantmentType>()
+        {
+            EnchantmentType.BaneOfArthropods,
+            EnchantmentType.CurseOfVanishing,
+            EnchantmentType.FireAspect,
+            EnchantmentType.Knockback,
+            EnchantmentType.Looting,
+            EnchantmentType.Mending,
+            EnchantmentType.Sharpness,
+            EnchantmentType.Smite,
+            EnchantmentType.SweepingEdge,
+            EnchantmentType.Unbreaking
+        },
+        DefaultEnchantmentTypes: new List<EnchantmentType>()
+        {
+            EnchantmentType.FireAspect,
+            EnchantmentType.Knockback,
+            EnchantmentType.Looting,
+            EnchantmentType.Mending,
+            EnchantmentType.Sharpness,
+            EnchantmentType.SweepingEdge,
+            EnchantmentType.Unbreaking
+        });
+
+    public static ItemType Axe = new(
+        FriendlyName: "Axe",
+        CompatibleEnchantmentTypes: new List<EnchantmentType>()
+        {
+            EnchantmentType.BaneOfArthropods,
+            EnchantmentType.CurseOfVanishing,
+            EnchantmentType.Efficiency,
+            EnchantmentType.Fortune,
+            EnchantmentType.Mending,
+            EnchantmentType.Sharpness,
+            EnchantmentType.SilkTouch,
+            EnchantmentType.Smite,
+            EnchantmentType.Unbreaking
+        },
+        DefaultEnchantmentTypes: new List<EnchantmentType>()
+        {
+            EnchantmentType.Efficiency,
+            EnchantmentType.Mending,
+            EnchantmentType.Sharpness,
+            EnchantmentType.SilkTouch,
+            EnchantmentType.Unbreaking
+        });
+
+    public static ItemType Pickaxe = new(
+        FriendlyName: "Pickaxe",
+        CompatibleEnchantmentTypes: new List<EnchantmentType>()
+        {
+             EnchantmentType.CurseOfVanishing,
+             EnchantmentType.Efficiency,
+             EnchantmentType.Fortune,
+             EnchantmentType.Mending,
+             EnchantmentType.SilkTouch,
+             EnchantmentType.Unbreaking
+        },
+        DefaultEnchantmentTypes: new List<EnchantmentType>()
+        {
+             EnchantmentType.Efficiency,
+             EnchantmentType.Mending,
+             EnchantmentType.SilkTouch,
+             EnchantmentType.Unbreaking
+        });
+
+    public static ItemType Shovel = new(
+        FriendlyName: "Shovel",
+        CompatibleEnchantmentTypes: new List<EnchantmentType>()
+        {
+            EnchantmentType.CurseOfVanishing,
+            EnchantmentType.Efficiency,
+            EnchantmentType.Fortune,
+            EnchantmentType.Mending,
+            EnchantmentType.SilkTouch,
+            EnchantmentType.Unbreaking
+        },
+        DefaultEnchantmentTypes: new List<EnchantmentType>()
+        {
+            EnchantmentType.Efficiency,
+            EnchantmentType.Mending,
+            EnchantmentType.SilkTouch,
+            EnchantmentType.Unbreaking
+        });
+
+    public static ItemType Hoe = new(
+        FriendlyName: "Hoe",
+        CompatibleEnchantmentTypes: new List<EnchantmentType>()
+        {
+             EnchantmentType.CurseOfVanishing,
+             EnchantmentType.Efficiency,
+             EnchantmentType.Fortune,
+             EnchantmentType.Mending,
+             EnchantmentType.SilkTouch,
+             EnchantmentType.Unbreaking
+        },
+        DefaultEnchantmentTypes: new List<EnchantmentType>()
+        {
+            EnchantmentType.Efficiency,
+             EnchantmentType.Mending,
+             EnchantmentType.SilkTouch,
+             EnchantmentType.Unbreaking
+        });
+
+    public static ItemType Bow = new(
+        FriendlyName: "Bow",
+        CompatibleEnchantmentTypes: new List<EnchantmentType>()
+        {
+            EnchantmentType.CurseOfVanishing,
+            EnchantmentType.Flame,
+            EnchantmentType.Infinity,
+            EnchantmentType.Mending,
+            EnchantmentType.Power,
+            EnchantmentType.Punch,
+            EnchantmentType.Unbreaking
+        },
+        DefaultEnchantmentTypes: new List<EnchantmentType>()
+        {
+            EnchantmentType.Flame,
+            EnchantmentType.Infinity,
+            EnchantmentType.Power,
+            EnchantmentType.Punch,
+            EnchantmentType.Unbreaking
+        });
+
+    public static ItemType FishingRod = new(
+        FriendlyName: "FishingRod",
+        CompatibleEnchantmentTypes: new List<EnchantmentType>()
+        {
+            EnchantmentType.CurseOfVanishing,
+            EnchantmentType.LuckOfTheSea,
+            EnchantmentType.Lure,
+            EnchantmentType.Mending,
+            EnchantmentType.Unbreaking
+        },
+        DefaultEnchantmentTypes: new List<EnchantmentType>()
+        {
+            EnchantmentType.LuckOfTheSea,
+            EnchantmentType.Lure,
+            EnchantmentType.Mending,
+            EnchantmentType.Unbreaking
+        });
+
+    public static ItemType Trident = new(
+        FriendlyName: "Trident",
+        CompatibleEnchantmentTypes: new List<EnchantmentType>()
+        {
+            EnchantmentType.Channeling,
+            EnchantmentType.CurseOfVanishing,
+            EnchantmentType.Impaling,
+            EnchantmentType.Loyalty,
+            EnchantmentType.Mending,
+            EnchantmentType.Riptide,
+            EnchantmentType.Unbreaking
+        },
+        DefaultEnchantmentTypes: new List<EnchantmentType>()
+        {
+            EnchantmentType.Channeling,
+            EnchantmentType.Impaling,
+            EnchantmentType.Loyalty,
+            EnchantmentType.Mending,
+            EnchantmentType.Unbreaking
+        });
+
+    public static ItemType Crossbow = new(
+        FriendlyName: "Crossbow",
+        CompatibleEnchantmentTypes: new List<EnchantmentType>()
+        {
+            EnchantmentType.CurseOfVanishing,
+            EnchantmentType.Mending,
+            EnchantmentType.Multishot,
+            EnchantmentType.Piercing,
+            EnchantmentType.QuickCharge,
+            EnchantmentType.Unbreaking
+        },
+        DefaultEnchantmentTypes: new List<EnchantmentType>()
+        {
+            EnchantmentType.Mending,
+            EnchantmentType.Multishot,
+            EnchantmentType.QuickCharge,
+            EnchantmentType.Unbreaking
+        });
+
+    public static ItemType Shears = new(
+        FriendlyName: "Shears",
+        CompatibleEnchantmentTypes: new List<EnchantmentType>()
+        {
+            EnchantmentType.CurseOfVanishing,
+            EnchantmentType.Efficiency,
+            EnchantmentType.Mending,
+            EnchantmentType.Unbreaking
+        },
+        DefaultEnchantmentTypes: new List<EnchantmentType>()
+        {
+            EnchantmentType.Efficiency,
+            EnchantmentType.Mending,
+            EnchantmentType.Unbreaking
+        });
+
+    public static ItemType FlintAndSteel = new(
+        FriendlyName: "FlintAndSteel",
+        CompatibleEnchantmentTypes: new List<EnchantmentType>()
+        {
+            EnchantmentType.CurseOfVanishing,
+            EnchantmentType.Mending,
+            EnchantmentType.Unbreaking
+        },
+        DefaultEnchantmentTypes: new List<EnchantmentType>()
+        {
+            EnchantmentType.Mending,
+            EnchantmentType.Unbreaking
+        });
+
+    public static ItemType CarrotOnAStick = new(
+        FriendlyName: "CarrotOnAStick",
+        CompatibleEnchantmentTypes: new List<EnchantmentType>()
+        {
+            EnchantmentType.CurseOfVanishing,
+            EnchantmentType.Mending,
+            EnchantmentType.Unbreaking
+        },
+        DefaultEnchantmentTypes: new List<EnchantmentType>()
+        {
+            EnchantmentType.Mending,
+            EnchantmentType.Unbreaking
+        });
+
+    public static ItemType WarpedFungusOnAStick = new(
+        FriendlyName: "WarpedFungusOnAStick",
+        CompatibleEnchantmentTypes: new List<EnchantmentType>()
+        {
+            EnchantmentType.CurseOfVanishing,
+            EnchantmentType.Mending,
+            EnchantmentType.Unbreaking
+        },
+        DefaultEnchantmentTypes: new List<EnchantmentType>()
+        {
+            EnchantmentType.Mending,
             EnchantmentType.Unbreaking
         });
     
